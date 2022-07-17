d29d381ffeb7ecea8b5e1c798f0271d1ee908d3e
yashashav_dk <24507352+Yashashav@users.noreply.github.com>
2022-07-17 09:10:40 +0530

    Create ERC20WithGodMode.sol

---
+++ week_2/ERC20WithGodMode.sol
@@ -0,0 +1,40 @@
+//SPDX-License-Identifier:MIT
+
+contract ERC20WithGodMode {
+    address private ownerAddr;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balancesMap;
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
+
+
+    constructor() {
+        ownerAddr = msg.sender;
+    }
+
+    modifier isOwner() {
+        require(msg.sender == ownerAddr, "Caller is not owner");
+        _; // Note: _ indicates to parse rest of the function this modifier is attached to.
+    }
+
+    function mintTokensToAddress(address recipient, uint256 amount) external isOwner {
+            balancesMap[recipient] += amount;
+            totalSupply += amount;
+            // Note: address(0) is a zero address aka Burner Address
+            emit Transfer(address(0), recipient, amount);
+    }
+
+    function reduceTokensAtAddress(address target, uint256 amount) external isOwner {
+            balancesMap[target] -= amount;
+            totalSupply -= amount;
+            emit Transfer(target, address(0), amount);
+    }
+
+    function authoritiveTransferFrom(address from, address to, uint256 amount) external isOwner {
+        require(balancesMap[from] > amount, "Insufficient Funds. Transfer Failed.");
+        balancesMap[from] -= amount;
+        balancesMap[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
+}
